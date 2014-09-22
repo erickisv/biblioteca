b024fe2354ad826e1efd798d528b3fa39d8167d2
Erick Sanchez <erick_isv@hotmail.com>
2014-09-21 19:29:21 -0600

    Prueba numero dos

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
-        iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii
+        Esta es una prueba
         <?php
         
         ?>
